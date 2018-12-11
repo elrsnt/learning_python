90cb491c9aaddd474587bc32b4c12e55c889843c
elrsnt <36866779+elrsnt@users.noreply.github.com>
2018-12-11 00:20:21 -0200

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,6 +1,7 @@
 num1 = int(input('Digita um numero qualquer'))
 num2 = int(input("Digite agora + um numero"))
-num3 = int(input('Diigete mais um numero o ultimo prometo'))
-sum = num1 + num2 + num3
+#num3 = int(input('Diigete mais um numero o ultimo prometo'))
+#sum = num1 + num2 + num3
+sum = num1 + num2 
 #print('os valores são iguais aos que você digitou?', num1, num2, num3)
-print('A soma entre os numeros é igual à',sum)
+print('A soma entre os e  numeros {} e {} é igual à {}',.format(num1, num2, sum))
