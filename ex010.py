5350a5326290412621eec006517ff4fcf5dc2273
Elismar Cerqueura <elrsnt@gmail.com>
2025-01-05 21:41:54 -0300

    done

--- ex010.py
+++ ex010.py
@@ -1,1 +1,5 @@
 #ex010 - Crie um programa que leia quantos dinheiro a pessoa tem na carteira e mostre quandos dolares ela pode comprar considere U$$1.00=R$3.27
+amount = float(input("Quanto dinheiro vc tem na carteira para comprar dollar? R$"))
+quot = float(input("Qual a cotacao atual do dollar? U$"))
+dollar_qt = amount / quot
+print("Com o {:.2f} voce pode comprar {:.2f} dolares".format( amount, dollar_qt))
--- ex013.py
+++ ex013.py
@@ -1,1 +1,4 @@
 #ex013 - Faça um algoritmo que leia o salário de um funcionario e mostre seu no salário com um aumento de 15%
+salario_base = float(input('Qual seu salario atual? '))
+portagem_aumento = salario_base / 100 * 15
+print('Com o salario {} aplicando 15% de aumento o novo salario sera de {}'.format(salario_base, salario_base + portagem_aumento))
