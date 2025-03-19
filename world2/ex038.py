1975de18e2bd7448bc163fdfdadbc2cf8fcd0205
Elismar Cerqueura <elrsnt@gmail.com>
2025-03-18 22:15:59 -0300

    done

--- world2/ex038.py
+++ world2/ex038.py
@@ -4,3 +4,12 @@ Escreva um progrma que leia dois numeros inteiros e compare-os, mostrando na tel
 - O segundo valor e maior
 - Nao existe valor maior, os dois sao iguais. 
 '''
+first = int(input('Insira um valor: '))
+second = int(input('Insira outro valor: '))
+if first > second:
+    print('O primeiro valor "{}" é maior que o segundo valor "{}"'.format(first ,second))
+elif second > first:
+    print('O segundo valor "{}" é maior que o primeiro valor "{}"'.format(second, first))
+else:
+    first == second
+    print('O primeiro valor {} é igual ao segundo valor {}'.format(first, second))
