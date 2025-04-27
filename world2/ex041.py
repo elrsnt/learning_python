70ef99cb19d53e789bc59cfed35d41d37424a1ab
elismar <36866779+elrsnt@users.noreply.github.com>
2025-04-27 20:53:31 -0300

    Update ex041.py

--- world2/ex041.py
+++ world2/ex041.py
@@ -8,3 +8,13 @@ sua categoria, de acordo com a idade:
 - Ate 20 anos: Senior
 - Acima: Master  
 '''
+age = int(input('Qual a sua idade? '))
+if age <= 9:
+    print("O Atleta possui {} anos de idade e fará parte do grupo Mirim!".format(age))
+elif age < 14:
+    print("O atleta possui {} anos de idade e fará parte do grupo Infantil".format(age))
+elif age >= 15 and age <= 19:
+    print("O atleta possui {} anos de idede e fará parte do grupo Junior".format(age))
+else:
+    #age => 20
+    print("O Atleta possui {} e está acima de vinte anos e seu grupo é Master".format(age))
