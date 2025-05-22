6e7418ed345face6f50c2ff2034b0e577883bb73
elismar <elrsnt@gmail.com>
2025-05-21 22:08:46 -0300

    new_exe

--- world2/ex048.py
+++ world2/ex048.py
@@ -1,1 +1,4 @@
 '''Faca um programa que calcule a soma entre todos os numeros pares que sao multiplos de tres e que se encontram no intervalo de 1 ate 500'''
+for c in range(0,4):
+    print(c, end=' ')
+print('Acabou')
