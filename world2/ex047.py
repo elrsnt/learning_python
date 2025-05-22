c0cf51a9868c75cc7e39f2208bbe5868933372e9
elismar <36866779+elrsnt@users.noreply.github.com>
2025-05-21 22:38:42 -0300

    Update ex047.py

--- world2/ex047.py
+++ world2/ex047.py
@@ -1,1 +1,4 @@
 '''Crie um programa que mostre na tela todos os numeros pares que estao no intervalo entre 1 e 50.'''
+for number in range(2, 51, 2):
+    print(number, end=' ')
+print('End of program')
