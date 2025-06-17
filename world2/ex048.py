8f46dd93e2c160a50ae7c89f5b1a2c2c7a2fcc56
elismar <36866779+elrsnt@users.noreply.github.com>
2025-06-16 21:06:39 -0300

    Update ex048.py

--- world2/ex048.py
+++ world2/ex048.py
@@ -1,1 +1,8 @@
 '''Faca um programa que calcule a soma entre todos os numeros pares que sao multiplos de tres e que se encontram no intervalo de 1 ate 500'''
+soma = 0
+cont = 0
+for c in range(1,501, 2):
+    if c % 3 == 0:
+        cont += 1
+        soma += c
+print('A soma de todos os {} valores solicitados tem o total de {} como soma'.format(cont, soma))
