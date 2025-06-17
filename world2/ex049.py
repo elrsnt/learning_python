17c5d64a85e636d288b5b2ea6d56826a5789cb22
elismar <36866779+elrsnt@users.noreply.github.com>
2025-06-16 21:07:14 -0300

    Update ex049.py

--- world2/ex049.py
+++ world2/ex049.py
@@ -1,2 +1,8 @@
 '''Refaca o desafio 009, mostrando a tabuade de um numero que o usuario escolher
 mas agora utilizando laco for'''
+#ex009 - Faça um programa leia um numero inteiro qualquer e imprima a sua tabuada na tela.
+n = int(input("Digite um número para a tabuada: "))
+print("-" * 12)
+for c in range(1, 11):
+    print("{} x {:2} = {:2}".format(n, c, n*c))
+print("-" * 12)
