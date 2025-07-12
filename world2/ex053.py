0c8c4f974e5963a7550122735870b0f6212a8ef1
elismar <36866779+elrsnt@users.noreply.github.com>
2025-07-12 19:50:24 -0300

    Update ex053.py

--- world2/ex053.py
+++ world2/ex053.py
@@ -10,3 +10,15 @@ a torre da derrota
 o lobo ama o lobo
 anotaram a data da maratona
 '''
+word = str(input('Digite uma frase: ')).strip().upper()
+words = word.split()
+together = ''.join(words)
+'''print('Você digitou a frase {}'.format(word))'''
+reverse = ''
+for letra in range(len(together) - 1, -1, -1):
+    reverse += together[letra]
+print('O inverso de {} é {}'.format(together, reverse))
+if reverse == together:
+    print('\nTemos um palindromo')
+else:
+    print('\nNão é um palindromo')
