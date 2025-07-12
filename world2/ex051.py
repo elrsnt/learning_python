e080a130175acab1171a673f0799411908ad540a
elismar <36866779+elrsnt@users.noreply.github.com>
2025-07-12 19:49:47 -0300

    Update ex051.py

--- world2/ex051.py
+++ world2/ex051.py
@@ -2,3 +2,9 @@
 Desenvolva um programa que leia o primeiro termo e a razao de uma PA (Progressa Aritimetica).
 No final, mostre os 10 primeiros termos dessa progressa.
 '''
+first = int(input('Digite um termo: '))
+second = int(input('Digite a razao: '))
+decimo = first + (10 - 1) * second
+for c in range(first, decimo + second, second):
+    print("{}".format(c), end='-> ')
+print('Acabou!')
