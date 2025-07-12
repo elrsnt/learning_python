c968b32dbc899e4a6396722745fb81b86671206c
elismar <36866779+elrsnt@users.noreply.github.com>
2025-07-12 19:50:40 -0300

    Update ex054.py

--- world2/ex054.py
+++ world2/ex054.py
@@ -4,3 +4,16 @@ No final mostre quantas pessoas ainda nao atingiram a maioridade
 e quantas ja sao maiores
 OBS: Condirar a maior idade de 21 anos
 '''
+from datetime import date
+nowadays = date.today().year
+totmaior = 0
+totmenor = 0
+for person in range(1, 8):
+    nasc = int(input('Qual ano que a {} pessoa nasceu? '.format(person)))
+    age = nowadays - nasc
+    if age >= 21:
+        totmaior += 1
+    else:
+        totmenor +=1
+print('{} São menores de idade'.format(totmenor))
+print('{} são maiores de idade'.format(totmaior))
